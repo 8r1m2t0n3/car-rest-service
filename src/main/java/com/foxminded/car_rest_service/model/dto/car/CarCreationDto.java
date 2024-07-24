d890f462d50a20bac4cbcf33320bd27d1d733665
HackerMan_3.0 <diakov.nv148@gmail.com>
2024-07-25 00:06:17 +0200

    lombok annotations added

--- src/main/java/com/foxminded/car_rest_service/model/dto/car/CarCreationDto.java
+++ src/main/java/com/foxminded/car_rest_service/model/dto/car/CarCreationDto.java
@@ -2,22 +2,25 @@ package com.foxminded.car_rest_service.model.dto.car;
 
 import com.foxminded.car_rest_service.model.dto.category.CategoryCreationDto;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import java.time.Year;
 import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Builder
 @Setter
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 public class CarCreationDto {
   private String objectId;
   @NotBlank private String brand;
   @NotBlank private String model;
   @NotNull private Year year;
-  private List<CategoryCreationDto> categories;
+  @NotEmpty private List<CategoryCreationDto> categories;
 }
--- src/main/java/com/foxminded/car_rest_service/model/dto/category/CategoryCreationDto.java
+++ src/main/java/com/foxminded/car_rest_service/model/dto/category/CategoryCreationDto.java
@@ -3,12 +3,14 @@ package com.foxminded.car_rest_service.model.dto.category;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Builder
 @Setter
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 public class CategoryCreationDto {
   private String name;
 }

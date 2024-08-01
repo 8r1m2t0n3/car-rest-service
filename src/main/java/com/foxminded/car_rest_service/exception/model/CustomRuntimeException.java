6cf0ee4903c9592d6996a6ccc8eb4627c6ea15e8
HackerMan_3.0 <diakov.nv148@gmail.com>
2024-08-01 13:10:23 +0200

    GlobalExceptionHandler implemented

--- src/main/java/com/foxminded/car_rest_service/exception/model/CustomRuntimeException.java
+++ src/main/java/com/foxminded/car_rest_service/exception/model/CustomRuntimeException.java
@@ -10,44 +10,21 @@ import org.springframework.http.HttpStatus;
 @Getter
 public abstract class CustomRuntimeException extends RuntimeException {
 
-  /** The exception message. */
   protected String exceptionMessage;
 
-  /** The HTTP status associated with the exception. */
   protected HttpStatus httpStatus;
 
-  /**
-   * Constructs a new custom exception with the specified cause and HTTP status.
-   *
-   * @param cause the cause (which is saved for later retrieval by the {@link #getCause()} method)
-   * @param httpStatus the HTTP status associated with the exception
-   */
   public CustomRuntimeException(Throwable cause, HttpStatus httpStatus) {
     super(cause);
     this.httpStatus = httpStatus;
   }
 
-  /**
-   * Constructs a new custom exception with the specified message and HTTP status.
-   *
-   * @param message the detail message (which is saved for later retrieval by the {@link
-   *     #getMessage()} method)
-   * @param httpStatus the HTTP status associated with the exception
-   */
   public CustomRuntimeException(String message, HttpStatus httpStatus) {
     super(message);
     this.exceptionMessage = message;
     this.httpStatus = httpStatus;
   }
 
-  /**
-   * Constructs a new custom exception with the specified message, cause, and HTTP status.
-   *
-   * @param message the detail message (which is saved for later retrieval by the {@link
-   *     #getMessage()} method)
-   * @param cause the cause (which is saved for later retrieval by the {@link #getCause()} method)
-   * @param httpStatus the HTTP status associated with the exception
-   */
   public CustomRuntimeException(String message, Throwable cause, HttpStatus httpStatus) {
     super(message, cause);
     this.exceptionMessage = message;
